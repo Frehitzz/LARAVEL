d7b310eadb2bf57cc2ea2133c6d4fa609e5663d4
Frehitzz <fritzharlydegamo@gmail.com>
2026-02-26 11:37:08 +0800

    learned layout files how to use components in diferent blade page

--- resources/views/nav/about.blade.php
+++ resources/views/nav/about.blade.php
@@ -1,22 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>About</title>
-        @vite("resources/css/app.css")
-    </head>
-    <body class="min-h-screen">
-        <nav class="flex justify-between bg-red-300 px-10 py-4">
-            <h1 class="text-2xl">My Site</h1>
-            <ul class="flex text-2xl gap-7">
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
-            </ul>
-        </nav>
-        <main class="flex items-center justify-center items-center">
-            <h1 class="text-3xl">This is About page</h1>
-        </main>
-    </body>
-</html>
+<x-layout>
+    <main class="flex items-center justify-center">
+        <h1 class="text-3xl">This is About page</h1>
+    </main>
+</x-layout>
