97c2cb4728ad2d606386fa9a2ab3709e9cdefeff
Frehitzz <fritzharlydegamo@gmail.com>
2026-02-26 21:49:39 +0800

    not saved

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 {{-- THIS IS A DEFAULT TITLE NAME FOR THOS WHO DONT HAVE TITLE --}}
 
 {{--
@@ -6,27 +5,15 @@
     "title" => "My-website",
     ])
 --}}
-=======
-@props([
-    'title' => 'Laracasts'
-])
->>>>>>> d7b310eadb2bf57cc2ea2133c6d4fa609e5663d4
 <!DOCTYPE html>
 <html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<<<<<<< HEAD
         <title>{{ $title }}</title>
         @vite("resources/css/app.css")
     </head>
     <body>
-=======
-        <title>{{$title}}</title>
-        @vite("resources/css/app.css")
-    </head>
-    <body class="">
->>>>>>> d7b310eadb2bf57cc2ea2133c6d4fa609e5663d4
         <nav class="flex justify-between bg-red-300 px-10 py-4">
             <h1 class="text-2xl">My Site</h1>
             <ul class="flex text-2xl gap-7">
@@ -37,13 +24,7 @@
         </nav>
 
         <main>
-<<<<<<< HEAD
             {{ $slot }}
         </main>
-=======
-           {{$slot}}
-        </main>
-        
->>>>>>> d7b310eadb2bf57cc2ea2133c6d4fa609e5663d4
     </body>
 </html>
